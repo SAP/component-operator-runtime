3e3a86b2218f7fec7afa25d2e43f5b5d45f44a76
cbarbian-sap <52255556+cbarbian-sap@users.noreply.github.com>
2025-08-21 17:46:46 +0200

    Auto-orphan foreign dependents on deletion (#317)
    * auto-orphan foreign dependents on deletion
    
    * refactoring
    
    * add brackets
    
    * bump versions
    
    * fix vet error
    
    * update makefile

---
+++ hack/tools.go
@@ -0,0 +1,13 @@
+//go:build tools
+// +build tools
+
+/*
+SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and component-operator-runtime contributors
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package tools
+
+import (
+	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
+)
