38b0c5803343d3085f4dc050cbb2c12be9a058f8
Christoph Barbian <christoph.barbian@sap.com>
2026-06-23 23:18:44 +0200

    add more tests

--- internal/fileutils/find_test.go
+++ internal/fileutils/find_test.go
@@ -6,9 +6,11 @@ SPDX-License-Identifier: Apache-2.0
 package fileutils_test
 
 import (
-	"fmt"
 	"io/fs"
 	"os"
+	"strings"
+
+	"github.com/sap/go-generics/slices"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -19,16 +21,131 @@ import (
 var _ = Describe("testing: find.go", func() {
 
 	var fsys fs.FS
+	var directories []string
+	var files []string
+	var symlinks []string
 
 	BeforeEach(func() {
 		fsys = os.DirFS("testdata/fs")
+		directories = []string{
+			"d",
+			"d/d",
+			"e",
+			"e/e",
+			"e/e/e",
+		}
+		files = []string{
+			"a.x",
+			"d/a.y",
+			"d/b.y",
+			"d/d/c.x",
+			"d/d/a.y",
+			"d/d/a.x",
+			"d/d/b.x",
+		}
+		symlinks = []string{
+			"d/la.y",
+			"d/ld",
+		}
 	})
 
 	It("should find everything", func() {
-		files, err := fileutils.Find(fsys, "", "", 0, 0)
-		fmt.Println(files)
+		paths, err := fileutils.Find(fsys, "", "", 0, 0)
 		Expect(err).NotTo(HaveOccurred())
-		Expect(files).NotTo(BeEmpty())
+		Expect(paths).To(ConsistOf(concat(directories, files, symlinks)))
+
+		paths, err = fileutils.Find(fsys, ".", "*", fileutils.FileTypeAny, 0)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(paths).To(ConsistOf(concat(directories, files, symlinks)))
+	})
+
+	It("should find directories", func() {
+		paths, err := fileutils.Find(fsys, "", "", fileutils.FileTypeDir, 0)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(paths).To(ConsistOf(concat(directories)))
+	})
+
+	It("should find files", func() {
+		paths, err := fileutils.Find(fsys, "", "", fileutils.FileTypeRegular, 0)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(paths).To(ConsistOf(concat(files)))
+	})
+
+	It("should find symlinks", func() {
+		paths, err := fileutils.Find(fsys, "", "", fileutils.FileTypeSymlink, 0)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(paths).To(ConsistOf(concat(symlinks)))
+	})
+
+	It("should find files or symlinks", func() {
+		paths, err := fileutils.Find(fsys, "", "", fileutils.FileTypeRegular|fileutils.FileTypeSymlink, 0)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(paths).To(ConsistOf(concat(files, symlinks)))
+	})
+
+	It("should find everything in a subdirectory", func() {
+		paths, err := fileutils.Find(fsys, "d", "", 0, 0)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(paths).To(ConsistOf(havingPrefix(concat(directories, files, symlinks), "d/")))
+	})
+
+	It("should honor namePattern", func() {
+		paths, err := fileutils.Find(fsys, "", "*.x", fileutils.FileTypeRegular, 0)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(paths).To(ConsistOf(havingSuffix(files, ".x")))
+	})
+
+	It("should honor maxDepth", func() {
+		paths, err := fileutils.Find(fsys, "", "", fileutils.FileTypeRegular, 1)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(paths).To(ConsistOf(havingMaxDepth(files, 1)))
+	})
+
+	It("should not follow symlinks", func() {
+		paths, err := fileutils.Find(fsys, "", "", fileutils.FileTypeAny, 0)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(havingPrefix(paths, "d/ld/")).To(BeEmpty())
+	})
+
+	It("should allow dir to be a symlink", func() {
+		paths, err := fileutils.Find(fsys, "d/ld", "", fileutils.FileTypeAny, 0)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(paths).To(ConsistOf(havingPrefix(replacingPrefix(concat(directories, files, symlinks), "d/d/", "d/ld/"), "d/ld/")))
 	})
 
 })
+
+func concat[T any](slices ...[]T) []T {
+	var result []T
+	for _, slice := range slices {
+		result = append(result, slice...)
+	}
+	return result
+}
+
+func havingPrefix(paths []string, prefix string) []string {
+	return slices.Select(paths, func(path string) bool {
+		return strings.HasPrefix(path, prefix)
+	})
+}
+
+func havingSuffix(paths []string, suffix string) []string {
+	return slices.Select(paths, func(path string) bool {
+		return strings.HasSuffix(path, suffix)
+	})
+}
+
+func havingMaxDepth(paths []string, maxDepth int) []string {
+	return slices.Select(paths, func(path string) bool {
+		return strings.Count(path, "/") < maxDepth
+	})
+}
+
+func replacingPrefix(paths []string, prefix, replacement string) []string {
+	return slices.Collect(paths, func(path string) string {
+		if strings.HasPrefix(path, prefix) {
+			return replacement + path[len(prefix):]
+		}
+		return path
+	})
+}
